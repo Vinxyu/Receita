24e8ffee847a3146e6ebc59d450518b805635f1c
Vinicius Alves <aalvesviniciusgarcia.s@gmail.com>
2025-01-28 11:55:49 -0300

    Alterações Gerais

--- cozinhando/cozinha.js
+++ cozinhando/cozinha.js
@@ -1,8 +1,21 @@
-let adic = document.querySelector("h1");
+//Variaveis de id
+
 let remove = document.getElementById("remover");
 let ingredi = document.getElementById("ingredientes");
+let prep = document.getElementById("modoprep");
+let mode = document.getElementById("inputpreparo");
+let butao = document.getElementById("admp");
+//VARIAVEIS DE SELEÇÃO
+
 let container = document.querySelector("#container").innerHTML;
 let list ;
+let adic = document.querySelector("h1");
+let adic2 = document.querySelector("h2");
+let adic3 = document.querySelector("h3");
+let adic4 = document.querySelector("h4");
+
+//FUNÇÃO ADICIONAR
+
 function add()
 {
     let nome = prompt("Coloque o nome da sua receita aqui");
@@ -12,7 +25,9 @@ adic.innerHTML = nome;
 console.log(nome);
 remove.removeAttribute("disabled");
 ingredi.removeAttribute("disabled");
+prep.removeAttribute("disabled");
 container = document.querySelector("#container");
+
 }
 else if(nome == "")
 {
@@ -24,8 +39,11 @@ else
 }
 }
 
+//FUNÇÃO PARA INGREDIENTES
+
 function ing()
 {
+    adic3.innerHTML = "Lista de Ingredientes";
     let i = prompt("Coloque um ingrediente aqui");
 if(i !== null && i !== ""){
     let list = document.createElement("li");
@@ -42,12 +60,34 @@ else
 }
 }
 
+//FUNÇÃO DE MODO DE PREPARO
+
+function mod()
+{
+    adic2.innerHTML = "Modo de Preparo";
+    mode.removeAttribute("disabled");
+    prep.setAttribute("disabled", "");
+    butao.removeAttribute("disabled");
+}
+
+//ADICIONAR TEXTO
+
+function adm()
+{
+
+}
+
+//FUNÇÃO REMOVER
 function rem()
 {   
     container.innerHTML = "";
     adic.innerHTML = "";
+    adic2.innerHTML = "";
+    adic3.innerHTML = "";
     remove.setAttribute("disabled", "");
     ingredi.setAttribute("disabled", "");
+    mode.setAttribute("disabled", "");
+    butao.setAttribute("disabled", "");
     console.log(remove);
 }
 
